fcc407acaa4b32853c05a13577a59e9c5f9fe3cd
galvarado <guillermoalvarado89@gmail.com>
2021-10-25 13:19:13 -0500

    FIXING: Upgrading Kind version to v0.11.1 (to get k8s v1.21.1)

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ kubectl cluster-info
 echo "**** End installing kubectl"
 
 # Install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 echo "**** End installing kind"
